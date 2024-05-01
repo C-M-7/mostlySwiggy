addbd82a57a3dacf454dbd31f24cc58931878c0d
C-M-7 <chiranm007@gmail.com>
2024-05-01 18:29:14 +0530

    Added the Sidebar to the whole

--- frontend/src/Components/LSidebar.jsx
+++ frontend/src/Components/LSidebar.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 
-const LSidebar = () =>(
+const LSidebar = ({ isOpen, isClose }) => {
+  return (
     <>
-        <div className="h-screen w-[40%] top-0 left-0 fixed bg-white flex-col">
-            <div className="h-[20%] m-10">
-                <div>Cross</div>
-                <div>
-                    <input className='h-10 w-[100%] border border-gray-400 pl-2 pr-20 mt-6 focus:outline-none focus:border-orange-400' type="text" placeholder="Search for area, street name..."/>    
-                </div>
+        <div className={`h-screen w-[40%] shadow-md top-0 left-0 fixed bg-white flex-col transition-transform duration-300  ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+          <div className="h-[20%] m-10">
+            <div onClick={isClose} className="cursor-pointer hover:text-orange-400">Close</div>
+            <div>
+              <input
+                className="h-10 w-[100%] border border-gray-400 pl-2 pr-20 mt-6 focus:outline-none focus:border-orange-400"
+                type="text"
+                placeholder="Search for area, street name..."
+              />
             </div>
-            <div className="h-[20%] m-10">
-                Location Accessibility
-            </div>
+          </div>
+          <div className="h-[20%] m-10 border border-gray-400 pl-2 pt-2">
+            Location Accessibility
+          </div>
         </div>
     </>
-)
+  );
+};
 
 export default LSidebar;
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import LSidebar from "./LSidebar";
 
+const Navbar = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-const Navbar = () => (
+  const toggleLSideBar = () =>{
+    setSidebarOpen(!sidebarOpen);
+  }
+
+
+  return(
   <>
     <div className="navbar flex justify-between items-center text-s bg-white shadow-md font-bold">
       <div className="flex space-x-12 mx-4 items-center">
         <div className="navfeatures py-6 cursor-pointer hover:text-orange-400">SwiggyLogo</div>
-        <div className="navfeatures py-6 cursor-pointer  hover:text-orange-400">User Location</div>
+        <div className="navfeatures py-6 cursor-pointer  hover:text-orange-400" onClick={toggleLSideBar}>User Location</div>
       </div>
       <div className="flex space-x-12 mx-4">
         <div className="navfeatures py-6 cursor-pointer  hover:text-orange-400 ">Swiggy Corporate</div>
@@ -18,8 +25,9 @@ const Navbar = () => (
         <div className="navfeatures py-6 cursor-pointer  hover:text-orange-400">Cart</div>
       </div>
     </div>
-    <LSidebar/>
+    <LSidebar isOpen={sidebarOpen} isClose={toggleLSideBar}/>
   </>
-);
+  )
+};
 
 export default Navbar;
