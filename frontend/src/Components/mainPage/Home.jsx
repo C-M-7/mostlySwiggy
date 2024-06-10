cfff387bd81dd98f2300f1c3e136e54b8cd220d7
C-M-7 <chiranm007@gmail.com>
2024-06-10 19:39:18 +0530

    sorted the routing feature

---
+++ frontend/src/Components/mainPage/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import CardStack from "./CardStack";
+import Footer from "./Footer";
+import Main2Carousel from "./Main2Carousel";
+import MainCarousel from "./MainCarousel";
+import MainOptions2 from "./MainOption2";
+import MainOptions1 from "./MainOptions1";
+import Navbar from "./Navbar";
+
+function Home() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div>
+        <div className="px-[15%] pt-20">
+          <MainCarousel />
+          <hr className="mt-8" />
+          <Main2Carousel />
+          <hr className="mt-8" />
+          <CardStack />
+        </div>
+        <div className="px-[10%]">
+          <hr />
+          <MainOptions1 />
+          <MainOptions2 />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
--- frontend/src/Components/mainPage/Navbar.jsx
+++ frontend/src/Components/mainPage/Navbar.jsx
@@ -1,31 +1,22 @@
 import React, { useState } from "react";
 import RSidebar from "./RSidebar";
 import { Link } from "react-router-dom";
-import LSidebar from "./LSidebar";
 
 const Navbar = () => {
-  const [LsidebarOpen, setLsidebarOpen] = useState(false);
-  const toggleLSideBar = () => setLsidebarOpen(!LsidebarOpen)
   const [RsidebarOpen, setRsidebarOpen] = useState(false);
   const toggleRSideBar = () => setRsidebarOpen(!RsidebarOpen)
 
   return(
     <>
     <div >
-    <LSidebar isOpen={LsidebarOpen} isClose={toggleLSideBar}/>
     <RSidebar isOpen={RsidebarOpen} isClose={toggleRSideBar}/>
-    {
-      LsidebarOpen &&
-      <div className="fixed bg-gray-400 bg-opacity-30 h-screen w-screen backdrop-filter backdrop-blur-sm z-30" onClick={toggleLSideBar}></div>
-    }
     {
       RsidebarOpen &&
       <div className="fixed bg-gray-400 bg-opacity-30 h-screen w-screen backdrop-filter backdrop-blur-sm z-30" onClick={toggleRSideBar}></div>
     }
-    <div className={`flex justify-between items-center text-s z-10 bg-white shadow-md font-bold fixed w-full ${LsidebarOpen ? 'backdrop-filter' : ''}`}>
+    <div className={`flex justify-between items-center text-s z-10 bg-white shadow-md font-bold fixed w-full`}>
       <div className="flex space-x-12 mx-4 items-center">
-        <Link to='/' className="navfeatures py-6 cursor-pointer hover:text-orange-400 hover:scale-110">SwiggyLogo</Link>
-        <div className="navfeatures py-6 cursor-pointer  hover:text-orange-400" onClick={toggleLSideBar}>User Location</div>
+        <Link to='/' className="navfeatures py-6 cursor-pointer hover:text-orange-400">SwiggyLogo</Link>
       </div>
       <div className="flex space-x-12 mx-4">
         <Link to='/search' className="navfeatures py-6 cursor-pointer  hover:text-orange-400">Search</Link>
